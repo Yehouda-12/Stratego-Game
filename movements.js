465a834d3955df786d439fefa3c8e41c394b1574
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-09 10:15:15 +0200

    updating the available moves and another function to move the player if null

--- movements.js
+++ movements.js
@@ -1,78 +1,46 @@
 function error() {
     console.log("Sorry, Can't go there")
 }
-function move(board, player, location1, location2) {
-    player.location = board[location1][location2]
+function checkTile(board, row, col) {
+    return board[row][col]
 }
+function move(board, player, row, col) {
+    let beforeMove = [player.row, player.col]
+    if (checkTile(board, row, col) === 0) {
+        [player.row, player.col] = [row, col]
+        board[row][col] = player
+        board[beforeMove[0]][beforeMove[1]] = null
+        } else {
+            battel()
+        }
+    return board
 
-function getBoarders(board, player) {
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board.length; j++) {
-            if (i === 0 && j === 0) {
-                console.log("You'r available movements are [down, right]")
-                let choice = input("Enter your choice")
-                if (choice === "left" || choice === "up") {
-                    error()
-                } else {
-                    move(board, player, i, j)
-                }
-            } else if (i === 0 && j < board.length - 1) {
-                console.log("You'r available movements are [down, right, left]")
-                let choice = input("Enter your choice")
-                if (choice === "up") {
-                    error()
-                } else {
-                    move(board, player, i, j)
-                }
-            } else if (i === 0 && j === board.length - 1) {
-                console.log("You'r available movements are [down, left]")
-                let choice = input("Enter your choice")
-                if (choice === "up" || choice === "right") {
-                    error()
-                } else {
-                    move(board, player, i, j)
-                }
-            } else if (i < board.length - 1 && j === 0) {
-                console.log("You'r available movements are [down, right, up]")
-                let choice = input("Enter your choice")
-                if (choice === "left") {
-                    error()
-                } else {
-                    move(board, player, i, j)
-                }
-            } else if (i < board.length - 1 && j === board.length - 1) {
-                console.log("You'r available movements are [down, up, left]")
-                let choice = input("Enter your choice")
-                if (choice === "right") {
-                    error()
-                } else {
-                    move(board, player, i, j)
-                }
-            } else if (i === board.length - 1 && j === 0) {
-                console.log("You'r available movements are [up, right]")
-                let choice = input("Enter your choice")
-                if (choice === "left" || choice === "down") {
-                    error()
-                } else {
-                    move(board, player, i, j)
-                }
-            } else if (i === board.length - 1 && j < board.length - 1) {
-                console.log("You'r available movements are [up, right, left]")
-                let choice = input("Enter your choice")
-                if (choice === "down") {
-                    error()
-                } else {
-                    move(board, player, i, j)
-                }
-            } else if (i === board.length - 1 && j === board.length - 1) {
-                console.log("You'r available movements are [up, left]")
-                let choice = input("Enter your choice")
-                if (choice === "down" || choice === "right") {
-                    error()
-                } else {
-                    move(board, player, i, j)
-                }
-            }
+}
+function getPossibleMoves(board, player) {
+    const possibleMoves = {};
+
+    const directions = {
+        up: { r: -1, c: 0 },
+        down: { r: 1, c: 0 },
+        left: { r: 0, c: -1 },
+        right: { r: 0, c: 1 }
+    };
+
+    for (const [dir, delta] of Object.entries(directions)) {
+        const newRow = player.row + delta.r;
+        const newCol = player.col + delta.c;
+
+        if (newRow >= 0 &&
+            newRow < board.length &&
+            newCol >= 0 &&
+            newCol < board[0].length) {
+            possibleMoves
+            [dir] = delta;
         }
     }
+
+    return possibleMoves;
 }
+
+
+
